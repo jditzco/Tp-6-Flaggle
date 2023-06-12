98b8ec0673ad83e1d74817badf59b0dda2192ce8
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-06-12 08:43:14 -0300

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import NavBarra from './Components/NavBarra'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <NavBarra/>
     </>
   )
 }
