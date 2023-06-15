779c72a38dcf7853c6a25421a8cd14e324c4f134
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-06-15 11:32:03 -0300

    Update App.jsx
    sdaas

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,46 @@
 import './App.css'
 import './Components/Flaggle.css'
-import React, {useState, useEffect } from "react"
+import React, {useState, useEffect, useRef } from "react"
 
 const App = () => {
-  const [Bandera, setBandera] = useState([{
-    bandera: null,
-    nombre: null,
-  }])
-  const [Abandera, setABandera] = useState(0)
+  const [listaBanderas, setListaBanderas] = useState([])
+  const [bandera, setBandera] = useState(0)
   const [puntos, setPuntos] = useState(0)
+  const input = useRef()
 
   useEffect(() => {
-    const obtenerAbanderas = () => {
-      fetch("https://countriesnow.space/api/v0.1/countries/flag/images")
-        .then(res => res.json())
-        .then(res => {
-          const updatedBandera = res.data.map(e => ({
-            bandera: e.flag,
-            nombre: e.name.toUpperCase()
-          }))
-          setBandera(updatedBandera)
-        })
-    }
-    obtenerAbanderas()
+    fetch("https://countriesnow.space/api/v0.1/countries/flag/images")
+    .then(res => res.json())
+    .then(res => setListaBanderas(res.data))
   }, [])
 
   useEffect(() => {
-    setABandera(Math.floor(Math.random() * Bandera.length))
-    console.log(Abandera)
-  }, [Bandera])
+    if (!listaBanderas) return
+    setBandera(listaBanderas[Math.floor(Math.random() * listaBanderas.length)])
+  }, [listaBanderas])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (document.getElementById("input-bandera").value.length > Bandera[Abandera].nombre.length * 0.50 ){
+    if (input.current.value.length > Math.ceil(bandera.name.length * 0.5)){
 
-      if (Bandera[Abandera].nombre.includes(document.getElementsByName("pais")[0].value.toUpperCase()) ) {
-        document.getElementsByName("pais")[0].value = ''
-        setABandera(Math.floor(Math.random() * Bandera.length))
-        setPuntos((e) => e + 10)
+      if (bandera.name.toUpperCase().includes(input.current.value.toUpperCase()) ) {
+        input.current.value = ''
+        setBandera(Math.floor(Math.random() * listaBanderas.length))
+        setPuntos(prevState => prevState + 10)
         console.log("correcto")
-        console.log(document.getElementsByName("pais")[0].value)
+        console.log(input.current.value)
       }
       else {
-        document.getElementsByName("pais")[0].value = ''
+        input.current.value = ''
         setPuntos((e) => e - 1)
         console.log("incorrecto")
-        console.log(document.getElementsByName("pais")[0].value)
+        console.log(input.current.value)
       }
 
     }
     else {
-      console.log("error no puede tener tan pocas letras")
+      console.log(bandera)
+      console.log(input.current.value)
     }
     
   }
@@ -60,8 +50,8 @@ const App = () => {
       <h2>¿Sabes de donde es esta bandera?</h2>
       <div className="banderita" ></div>
       <form onSubmit={handleSubmit}>
-        <div><img src={Bandera[Abandera].bandera} /></div>
-        <input type="text" id="input-bandera" name="pais" className="u-full-width" placeholder= "Que bandera es?" />
+        <div><img src={bandera?.flag} /></div>
+        <input type="text" ref={input} id="input-bandera" name="pais" className="u-full-width" placeholder= "Que bandera es?" />
         <button type="submit" className="u-full-width button-primary">Submit</button>
         <h3>Puntos: {puntos}</h3>
       </form>
